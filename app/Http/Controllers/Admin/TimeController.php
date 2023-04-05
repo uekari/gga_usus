b65ea9cbec6b36e6c6c31a7c68e4123daf8a863a
soejimayuka <wy816.615@gmail.com>
2023-04-05 10:16:11 +0900

    修正

--- app/Http/Controllers/Admin/TimeController.php
+++ app/Http/Controllers/Admin/TimeController.php
@@ -45,19 +45,25 @@ class TimeController extends Controller
         $time->is_move = $request->is_move;
         $time->risk_title1 = $request->risk_title1;
         $time->risk_content1 = $request->risk_content1;
-        $img1 = $request->file('risk_img1');
-        $path = $img1->store('risk_img1','public');
-        $time->risk_img1 = $path;
+        if($request->hasFile('risk_img1')) {
+            $img1 = $request->file('risk_img1');
+            $path = $img1->store('risk_img1','public');
+            $time->risk_img1 = $path;
+        }
         $time->risk_title2 = $request->risk_title2;
         $time->risk_content2 = $request->risk_content2;
-        $img2 = $request->file('risk_img2');
-        $path = $img2->store('risk_img2','public');
-        $time->risk_img2 = $path;
+        if($request->hasFile('risk_img2')) {
+            $img2 = $request->file('risk_img2');
+            $path = $img2->store('risk_img2','public');
+            $time->risk_img2 = $path;
+        }
         $time->risk_title3 = $request->risk_title3;
         $time->risk_content3 = $request->risk_content3;
-        $img3 = $request->file('risk_img3');
-        $path = $img3->store('risk_img3','public');
-        $time->risk_img3 = $path;
+        if($request->hasFile('risk_img3')) {
+            $img3 = $request->file('risk_img3');
+            $path = $img3->store('risk_img3','public');
+            $time->risk_img3 = $path;
+        }
 
 
         // dd($time);
