29e3ca5c6c73f15575859002ef7a95dd18607297
soejimayuka <wy816.615@gmail.com>
2023-04-05 21:05:37 +0900

    行けるか？CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
 
   <!-- Scripts -->
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+  <link href=“https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css” rel=“stylesheet”>
+  
 </head>
 <body class="font-sans antialiased">
   <div class="min-h-screen bg-gray-100">
