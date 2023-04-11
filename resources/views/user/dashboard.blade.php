5ea4cea214ab30fdbff4a9ee1485fc783b40435c
soejimayuka <wy816.615@gmail.com>
2023-04-11 19:54:49 +0900

    name

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -3,17 +3,19 @@
   <div class="sm:mt-12 pb-12">
     <div class="max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12 ">
       <div class="bg-white overflow-hidden sm:rounded-lg min-height">
-        <div class="p-6 text-gray-900">
-          <h1>サポーター {{ Auth::user()->name }} 様</h1>
-          <p>この度、旅のサポートを引き受けて下さり</p>
-          <p>ありがとうございます。</p>
-          <p>安心安全に旅を楽しんでもらえるよう、</p>
-          <p>最後まで全力でサポートさせて頂きます！</p>
-          <p>少しでも分からないことなどございましたら、</p>
-          <p>いつでもご連絡下さい！</p>
-          <img src="http://www.ushikukankou.com/images2/others/ushikushi-goto/04.jpg" width="60%">
-          <p>〇〇会社</p>
-          <p>000-000-0000</p>
+        <div class="py-8 px-6 text-gray-900">
+          <h1 class="text-xl mb-4">サポーター {{ Auth::user()->name }} 様</h1>
+          <div class="text-sm mb-2">
+            <p class='mb-2'>この度、旅のサポートを引き受けて下さり<br>ありがとうございます。</p>
+            <p class='mb-2'>安心安全に旅を楽しんでもらえるように、<br>最後まで全力でサポートさせて頂きます！</p>
+            <p>少しでも分からないことなどございましたら、<br>いつでもご連絡下さい！</p>
+          </div>
+          <div class="illust">
+            <img src="{{ asset('illust/top_message.png')}}">
+          </div>
+
+          <p class='mb-1'>SOY旅 Co.</p>
+          <p>TEL : <a href="tel:">000-000-0000</a></p>
         </div>
       </div>
     </div>
